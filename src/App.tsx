efebb9f5431b989fecb1939b8ab653bfa6d7596e
Melissa Vela <melissa@instarent.com.au>
2024-04-05 12:25:05 +1100

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Show, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+     <Grid
+templateAreas={{base: `"nav" "main"`,
+                lg: `"nav nav"
+                "aside main"`}}
+  gridTemplateRows={'50px 1fr 30px'}
+
+  h='200px'
+  gap='1'
+  color='blackAlpha.700'
+  fontWeight='bold'
+>
+ 
+  <GridItem pl='2' bg='pink.300' area={'nav'}>
+    Nav
+  </GridItem>
+
+<Show above="lg">
+  <GridItem pl='2' bg='orange.300' area={'aside'}>
+    Aside
+  </GridItem>
+
+</Show>
+
+  <GridItem pl='2' bg='green.300' area={'main'}>
+    Main
+  </GridItem>
+  
+</Grid>
+
+
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
